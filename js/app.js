cc7bcb5566a781d78cf2bc77a8d203345670f4a3
Grant Iverson <iversonga@gmail.com>
2018-03-26 02:29:32 -0400

    Got multiple pages functioning with jQuery

--- js/app.js
+++ js/app.js
@@ -24,32 +24,12 @@ $(document).ready(function() {
         $(this).fadeOut();
     });
 
-    /* Decides what to do when clicking on a link */
-    // $("a").click(function() {
-    //     var clickedHref = $(this).attr("href");                          /* Stores the href of clicked link in clickedHref                      */
-    //     var hrefTest = /http/i.test(clickedHref)                         /* does it include "http"                                              */
-    //                 || /mailto/i.test(clickedHref)                       /* does it include "mailto"                                            */
-    //                 || /tel/i.test(clickedHref)                          /* does it include "tel"                                               */
-    //                 || /sms/i.test(clickedHref)                          /* does it include "sms"?                                              */
-    //                 || /work/i.test(clickedHref)                         /* does it include "work"?                                             */
-    //     if (hrefTest) {
-    //         return;                                                      /* if it does include any of these, do nothing                         */
-    //     } else if ($(this).hasClass("return-to-top-button")) {           /* is the link the .return-to-top-button at the bottom of each .page,  */
-    //         event.preventDefault();
-    //         $("#mini-nav").slideUp();                                    /* hide the #mini-nav                                                  */
-    //     } else {
-    //         event.preventDefault();                                      /* otherwise, don't go to the link in the HTML                         */
-    //         $("#mini-nav").slideUp();                                    /* hide the #mini-nav                                                  */
-    //         $("main").fadeOut("", function() {
-    //             $(".page").hide();                                       /* hide the .page you were on                                          */
-    //             $("a.active").removeClass("active");                     /* remove the class .active from the #main-nav or #mini-nav link       */
-    //             $(clickedHref + "-main-nav").addClass("active").blur();  /* add the class .active to the #main-nav link                         */
-    //             $(clickedHref + "-mini-nav").addClass("active").blur()   /* add the class .active to the #mini-nav link                         */
-    //             $(clickedHref).show();                                   /* show the .page you are going to                                     */
-    //             $("main").fadeIn();
-    //         });
-    //     }
-    //     $("html, body").animate({scrollTop: "0px" });                    /* scroll the window to the top of the page                            */
-    // });
+    $("a").click(function() {
+        if ($(this).hasClass("return-to-top-button")) {      /* is the link the .return-to-top-button at the bottom of each .page,  */
+            //event.preventDefault();
+            $("#mini-nav").slideUp();                        /* hide the #mini-nav                                                  */
+            $("html, body").animate({scrollTop: "0px" });    /* scroll the window to the top of the page                            */
+        }
+    });
 
 });
