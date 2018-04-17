12d32f7e6492eec3b3b3b88ce2ed087e1da0ed0b
Grant Iverson <iversonga@gmail.com>
2018-04-16 22:36:15 -0400

    updated gulpfile to include 'dist' task

--- gulpfile.js
+++ gulpfile.js
@@ -4,22 +4,24 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     connect = require('gulp-connect'),
     uglify = require('gulp-uglify'),
+    eslint = require('gulp-eslint'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['build/js/hello.coffee'],
-    jsSources = ['build/js/*.js'],
-    sassSources = ['build/css/*.sass'],
+    scriptsSources = ['build/js/*.js'],
+    stylesSources = ['build/css/*.sass'],
     htmlSources = ['build/*.html'],
-    imgSources = ['build/img/*'],
+    imagesSources = ['build/img/*'],
     outputDir = 'dist';
 
 gulp.task('copy-html', function() {
   gulp.src(htmlSources)
     .pipe(gulp.dest(outputDir))
+    .pipe(connect.reload())
 });
 
-gulp.task('copy-img', function() {
-  gulp.src(imgSources)
+gulp.task('copy-images', function() {
+  gulp.src(imagesSources)
     .pipe(gulp.dest(outputDir + '/img'))
 });
 
@@ -28,8 +30,8 @@ gulp.task('copy-work-dir', function() {
     .pipe(gulp.dest(outputDir + '/work'))
 });
 
-gulp.task('sass', function() {
-  gulp.src(sassSources)
+gulp.task('styles', function() {
+  gulp.src(stylesSources)
     .pipe(sass({outputStyle: 'compressed'}))
       .on('error', gutil.log)
     .pipe(gulp.dest(outputDir + '/css'))
@@ -43,20 +45,27 @@ gulp.task('coffee', function() {
     .pipe(gulp.dest(outputDir + '/js'))
 });
 
-gulp.task('js', function() {
-  gulp.src(jsSources)
-    .pipe(uglify())
+gulp.task('scripts', function() {
+  gulp.src(scriptsSources)
     .pipe(concat('app.js'))
     .pipe(gulp.dest(outputDir + '/js'))
     .pipe(connect.reload())
 });
 
+gulp.task('scripts-dist', function() {
+  gulp.src(scriptsSources)
+    .pipe(uglify())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest(outputDir + '/js'))
+    .pipe(connect.reload())
+})
+
 gulp.task('watch', function() {
-  gulp.watch(coffeeSources, ['coffee']);
-  gulp.watch(jsSources, ['js']);
-  // gulp.watch(jsSources, ['lint']);
-  gulp.watch(sassSources, ['sass']);
   gulp.watch(htmlSources, ['html']);
+  gulp.watch(stylesSources, ['styles']);
+  gulp.watch(coffeeSources, ['coffee']);
+  gulp.watch(scriptsSources, ['scripts']);
+  gulp.watch(scriptsSources, ['lint']);
 });
 
 gulp.task('connect', function() {
@@ -66,26 +75,39 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('html', function() {
-  gulp.src(htmlSources)
-    .pipe(connect.reload())
+gulp.task('lint', () => {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['**/*.js','!node_modules/**'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint())
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
 });
 
-// gulp.task('lint', () => {
-//   // ESLint ignores files with "node_modules" paths.
-//   // So, it's best to have gulp ignore the directory as well.
-//   // Also, Be sure to return the stream from the task;
-//   // Otherwise, the task may end before the stream has finished.
-//   return gulp.src(['**/*.js','!node_modules/**'])
-//     // eslint() attaches the lint output to the "eslint" property
-//     // of the file object so it can be used by other modules.
-//     .pipe(eslint())
-//     // eslint.format() outputs the lint results to the console.
-//     // Alternatively use eslint.formatEach() (see Docs).
-//     .pipe(eslint.format())
-//     // To have the process exit with an error code (1) on
-//     // lint error, return the stream and pipe to failAfterError last.
-//     .pipe(eslint.failAfterError());
-// });
+gulp.task('dist', [
+  'copy-html',
+  'copy-images',
+  'styles',
+  'lint',
+  'scripts-dist'
+])
 
-gulp.task('default', ['html', 'coffee', 'js', 'sass', 'connect', 'watch', 'copy-html', 'copy-img', 'copy-work-dir']);
+gulp.task('default', [
+  'copy-html',
+  'copy-images',
+  'styles',
+  'lint',
+  'scripts',
+  'coffee',
+  'connect',
+  'watch',
+  'copy-work-dir'
+]);
