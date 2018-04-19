8186cfcd116bdba08175814a3124667c0bc16aee
Grant Iverson <iversonga@gmail.com>
2018-04-18 22:26:33 -0400

    Added project selector menu

--- gulpfile.js
+++ gulpfile.js
@@ -116,7 +116,7 @@ gulp.task('lint', function() {
   // So, it's best to have gulp ignore the directory as well.
   // Also, Be sure to return the stream from the task;
   // Otherwise, the task may end before the stream has finished.
-  return gulp.src(['**/*.js','!node_modules/**'])
+  return gulp.src(['**/*.js','!node_modules/**','!build/js/app.js'])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
     .pipe(eslint())
